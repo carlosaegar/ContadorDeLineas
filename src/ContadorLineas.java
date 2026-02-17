e5dd8418793645554978efac757f24ee83dfea11
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-17 14:50:15 +0100

    Update ContadorLineas.java

--- src/ContadorLineas.java
+++ src/ContadorLineas.java
@@ -1,13 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class ContadorLineas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Pedimos la palabra
+        System.out.print("Introduce la palabra que quieres buscar: ");
+        String palabraObjetivo = sc.nextLine().toLowerCase();
+
+        int contador = 0;
+
+
+        try (BufferedReader lector = new BufferedReader(new FileReader("datos.txt"))) {
+
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                if (linea.toLowerCase().contains(palabraObjetivo)) {
+                    contador++;
+                }
+            }
+
+            System.out.println("La palabra '" + palabraObjetivo + "' aparece en " + contador + " líneas.");
+
+        } catch (IOException e) {
+            System.out.println("Error: No se pudo leer el archivo " +e.getMessage());
+        } finally {
+            sc.close();
+        }
+    }
 }
